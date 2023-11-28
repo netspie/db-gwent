d97244bf7376759b4a521b952b3bb90f016dd7cc
NOUTBUK\dariu <dariusz.lusnia@gmail.com>
2023-11-28 11:33:05 +0100

    Add missing code

--- src/dbgwent-gameplay-client/src/app/page.tsx
+++ src/dbgwent-gameplay-client/src/app/page.tsx
@@ -1,16 +1,30 @@
 import Header from "@/components/Header";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Home() {
+  const [loading, setLoading] = useState(false)
 
-  const fetchDataFromApi = () => {
-    await fetch("api/games/game-1", {
-      headers: {
-        Accept: "application/json",
-        method: "GET"
-      }
-    })
+  const fetchDataFromApi = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch("api/games/game-1", {
+        headers: {
+          Accept: "application/json",
+          method: "GET"
+        }
+      })
+  
+      if (response) {
+        const data = response.json()
+        console.log(data)
+      } 
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
   }
 
   return (
